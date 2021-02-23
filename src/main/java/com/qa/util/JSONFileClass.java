4ae43427a45fb1ff7291b475987995ca2e5ae0be
jyotsana10492 <tojyotsanapanwar@gmail.com>
2021-02-23 16:58:46 +0530

    Build 1

---
+++ src/main/java/com/qa/util/JSONFileClass.java
@@ -0,0 +1,22 @@
+package com.qa.util;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Serializable;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.WebDriverException;
+
+@SuppressWarnings("serial")
+public class JSONFileClass extends WebDriverException implements Serializable {
+	// public static void main(String[] arg) throws IOException, ParseException {
+	public JSONObject readJson() throws IOException, ParseException {
+		JSONParser jsonParser = new JSONParser();
+		FileReader reader = new FileReader("./src/test/resources/JsonFiles/Companydata.json");
+		Object obj = jsonParser.parse(reader);
+		JSONObject userLogIn = (JSONObject) obj;
+		return userLogIn;
+	}
+}
---
+++ test-output-thread/report.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "a1b0e3d7-87a2-420a-b151-cd945f6820de",
+    "feature": "To test the Company functionality of Subsource",
+    "scenario": "Check Company deatails",
+    "start": 1614079491434,
+    "group": 14,
+    "content": "",
+    "tags": "",
+    "end": 1614079517620,
+    "className": "passed"
+  },
+  {
+    "id": "0d5ab9a4-3d46-4a54-b754-69fc1e699430",
+    "feature": "To test the Shoppage functionality of Subsource",
+    "scenario": "Check Shop deatails",
+    "start": 1614079491431,
+    "group": 15,
+    "content": "",
+    "tags": "",
+    "end": 1614079529087,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 14,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 15,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  }
+]);
+});
